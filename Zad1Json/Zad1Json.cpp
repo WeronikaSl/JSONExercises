bc1a571673714e29b338f7cdc0aad17c2673ae43
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-09-09 15:43:03 +0200

    Zad1BasicJSON

--- Zad1Json/Zad1Json.cpp
+++ Zad1Json/Zad1Json.cpp
@@ -1,20 +1,20 @@
-﻿// Zad1Json.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+﻿#include "json/json.hpp"
 #include <iostream>
+#include <fstream>
+
+using json = nlohmann::json;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    json j;
+    j["imie"] = "Weronika";
+    j["nazwisko"] = "Slowinska";
+    j["wiek"] = 23;
+    j["praca"]["biuro"] = "tlumaczka";
+    j["praca"]["szkola"] = "nauczycielka";
+    j["lista"] = { 1,2,3 };
+
+    std::ofstream o("ja.json");
+    o << std::setw(4) << j << std::endl; //to setw - ile whitespaces żeby to ładnie poformatować, bez tego po prostu w jednej linijce
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
